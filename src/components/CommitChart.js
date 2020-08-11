44a451268ff35938b509797ef53ea3b3d0fb4ab5
fengde <jun1st.feng@gmail.com>
2020-08-11 23:29:00 +0800

    show line chart

--- src/components/CommitChart.js
+++ src/components/CommitChart.js
@@ -1,31 +1,19 @@
-import { Bar } from "vue-chartjs";
+import { Line } from "vue-chartjs";
 
 export default {
-  extends: Bar,
+  extends: Line,
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
   mounted() {
     // Overwriting base render method with actual data.
-    this.renderChart({
-      labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ],
-      datasets: [
-        {
-          label: "GitHub Commits",
-          backgroundColor: "#f87979",
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
-        },
-      ],
-    });
+    this.renderChart(this.chartData, this.options);
   },
 };
--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="list row">
-    <div class="col-md-12">
-      <h4>Stock</h4>
-      <commit-chart />
+    <div class="list row">
+        <div class="col-md-12">
+            <h4>Stock</h4>
+            <commit-chart v-if="loaded" :chartData="chartData" :options="options" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -12,32 +12,74 @@ import CommitChart from "./CommitChart";
 import StockDataService from "../services/StockDataService";
 
 export default {
-  components: {
-    CommitChart,
-  },
-  name: "stock-detail",
-  data() {
-    return {
-      stock: null,
-      trades: [],
-    };
-  },
-  methods: {
-    fetch() {
-      let code = this.$route.params.code;
+    components: {
+        CommitChart,
+    },
+    name: "stock-detail",
+    data() {
+        return {
+            stock: null,
+            trades: [],
+            loaded: false,
+            chartData: null,
+            options: null,
+        };
+    },
+    methods: {
+        fetch() {
+            let code = this.$route.params.code;
 
-      StockDataService.findOne(code).then((response) => {
-        this.stock = response.data.stock;
-        this.trades = response.data.trades;
-      });
+            StockDataService.findOne(code).then((response) => {
+                this.chartData = {
+                    labels: [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December",
+                    ],
+                    datasets: [
+                        {
+                            label: "GitHub Commits",
+                            backgroundColor: "#f87979",
+                            data: [
+                                40,
+                                20,
+                                12,
+                                39,
+                                10,
+                                40,
+                                39,
+                                80,
+                                40,
+                                20,
+                                12,
+                                11,
+                            ],
+                        },
+                    ],
+                };
+
+                this.options = {};
+                this.stock = response.data.stock;
+                this.trades = response.data.trades;
+                this.loaded = true;
+            });
+        },
+
+        refresh() {},
     },
 
-    refresh() {},
-  },
-
-  mounted() {
-    console.log(this.$route.params.code);
-    this.fetch();
-  },
+    mounted() {
+        console.log(this.$route.params.code);
+        this.fetch();
+    },
 };
 </script>
