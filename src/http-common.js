68be08d85f924e2eee5df29eae99897618f9a923
fengde <jun1st.feng@gmail.com>
2020-08-16 10:08:47 +0800

    use 666666 style

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://13.75.114.205/api",
+  baseURL: "http://www.666666.style/api",
   headers: {
     "Content-type": "application/json",
   },
